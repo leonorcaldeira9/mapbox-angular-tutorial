8ed7382b7f6426752eaabd6e724fc28d851b391f
leonorcaldeira9 <leonorcaldeira2018@gmail.com>
2024-05-09 20:24:30 +0100

    map appeared

--- mapbox-angular-final/src/environments/environment.prod.ts
+++ mapbox-angular-final/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     mapbox: {
-        accessToken: '<TOKEN HERE>' // Your productionaccess token goes here
+        accessToken: 'pk.eyJ1IjoibGVvbm9yY2FsZGVpcmE5IiwiYSI6ImNsdnd1bHJ4cDF4NXcyam55M3V5ajJ2Nm8ifQ.rzNLGY_rSex4xQAN48yj1Q' // Your productionaccess token goes here
       }
   };
   
--- mapbox-angular-final/src/environments/environment.ts
+++ mapbox-angular-final/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     mapbox: {
-      accessToken: '<TOKEN HERE>' // Your access token goes here
+      accessToken: 'pk.eyJ1IjoibGVvbm9yY2FsZGVpcmE5IiwiYSI6ImNsdnd1bHJ4cDF4NXcyam55M3V5ajJ2Nm8ifQ.rzNLGY_rSex4xQAN48yj1Q' // Your access token goes here
     }
   };

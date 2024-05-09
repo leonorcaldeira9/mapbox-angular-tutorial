18da85e6eb0ea98af11248d40a2777ddf21a5de0
leonorcaldeira9 <125878438+leonorcaldeira9@users.noreply.github.com>
2024-05-09 09:51:58 +0100

    Delete mapbox-angular-basic directory

--- mapbox-angular-basic/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    mapbox: {
-        accessToken: '<TOKEN HERE>' // Your production access token goes here
-      }
-  };
-  
--- mapbox-angular-basic/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    mapbox: {
-      accessToken: '<TOKEN HERE>' // Your access token goes here
-    }
-  };
